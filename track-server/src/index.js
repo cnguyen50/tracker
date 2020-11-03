efd21f3a324592a92d83f2b02e245f7f557e26cb
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-02 16:54:24 -0800

    defined User schema

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
+require('./models/User')
+
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
 app.use(authRoutes)
 
 const monogoUri = process.env.DB_KEY;
@@ -16,7 +21,6 @@ mongoose.connect(monogoUri, {
 mongoose.connection.on('connected', () => {
   console.log("Connected to mongo")
 })
-
 
 mongoose.connection.on('error', (err) => {
   console.error("Connected to mongo", err)
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
 const router = express.Router()
 
 router.post('/signup', (req, res) => {
+  console.log(req.body)
   res.send("You've made a post request")
 })
 
