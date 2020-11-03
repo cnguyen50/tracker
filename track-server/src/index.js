d08fdf39ef62ef58a999aa0d6b7619ef2887e795
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-02 18:59:43 -0800

    Listing tracks for a particular user after sign in

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 require('./models/User')
+require('./models/Track')
 
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const bodyParser = require('body-parser')
 const requireAuth = require('./middlewares/requireAuth')
 
@@ -11,6 +13,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(authRoutes)
+app.use(trackRoutes)
 
 const monogoUri = process.env.DB_KEY;
 
