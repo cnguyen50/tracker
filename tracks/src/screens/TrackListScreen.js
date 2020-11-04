70abc0c3b78158a3420fd6e85a0198138aee986e
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-03 21:29:18 -0800

    added navigation onPress props to trackList and trackDetail

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 
-const TrackListScreen = () => {
+const TrackListScreen = ({ navigation }) => {
   return (
-    <Text style={{ fontSize: 48 }}> TrackList Screen</Text>
+    <View>
+      <Text style={{ fontSize: 48 }}> TrackList Screen</Text>
+      <Button 
+        title="Go to Track Detail"
+        onPress={() => navigation.navigate('TrackDetail')}
+      />
+    </View>
   )
 }
 
