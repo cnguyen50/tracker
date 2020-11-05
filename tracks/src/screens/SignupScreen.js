c6fedb29b29e827cfa4271855e4166effc47240c
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-04 17:58:20 -0800

    made API post request when signed up

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
   return (
     <View style={styles.container}>
       <Spacer>
@@ -29,14 +32,10 @@ const SignupScreen = ({ navigation }) => {
       />
       <Spacer>
         <Button
-          title="Signup"
-          onPress={() => navigation.navigate('Signin')}
+          title="Sign Up"
+          onPress={() => signup({ email, password })}
         />
       </Spacer>
-      {/* <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate('mainFlow')}
-      />  */}
     </View>
   )
 }
