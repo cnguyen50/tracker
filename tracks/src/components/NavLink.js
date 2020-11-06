146abb015714eaa3e44589e7cafbb4b2c2f759fd
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-05 20:24:17 -0800

    boilerplate for resuable NavLink

---
+++ tracks/src/components/NavLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import Spacer from '../components/Spacer'
+import { withNavigation } from 'react-navigation'
+
+const NavLink = () => {
+  return (
+
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default withNavigation(NavLink)
