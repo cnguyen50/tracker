2bfc6a404cd328a43544557221d2417a30561c85
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-05 20:40:09 -0800

    added routeName prop to navLink

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavLink from '../components/NavLink'
+import AuthForm from '../components/AuthForm'
 
 
 const SigninScreen = () => {
   return (
-    <Text style={{ fontSize: 48 }}> SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign in into your account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+      <NavLink 
+        text="Don't have an account? Sign up instead"
+        routeName="Signup"
+      />
+    </View>
   )
 }
 
-const styles = StyleSheet.create({})
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+})
 
 export default SigninScreen
