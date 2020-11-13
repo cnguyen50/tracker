239bf2bd6fc5a3357e3be8073e57c76fe520b790
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-12 17:34:12 -0800

    redirect signout button to sign up page

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -60,6 +60,7 @@ const signin = (dispatch) => async ({ email, password }) => {
 const signout = dispatch =>  async () => {
   await AsyncStorage.removeItem('token')
   dispatch({ type: 'signout'})
+  navigate('loginFlow')
 }
 
 
