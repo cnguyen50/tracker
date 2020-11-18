a61d074f4338fdfee6e542acc8ef8fcd4a78d2ff
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-17 21:50:36 -0800

    implemented logic to askl users to enable location services and had err render

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -6,10 +6,28 @@ import { Text } from 'react-native-elements'
 import { requestPermissionsAsync } from 'expo-location'
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null)
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2> Create a track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text>: null}
     </SafeAreaView>
   )
 }
