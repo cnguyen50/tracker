dbca4ea0500df798a8f1762b86b363d279046875
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-17 22:10:26 -0800

    imported watchPositionAsync and Accuracy from expo location to test mock points and show on terminal

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,19 +1,28 @@
+import '../_mockLocation'
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Map from '../components/Map'
 import { SafeAreaView } from 'react-navigation'
 import { Text } from 'react-native-elements'
-import { requestPermissionsAsync } from 'expo-location'
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'
 
 const TrackCreateScreen = () => {
   const [err, setErr] = useState(null)
 
   const startWatching = async () => {
     try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
+      // const { granted } = await requestPermissionsAsync();
+      // if (!granted) {
+      //   throw new Error('Location permission not granted');
+      // }
+      await requestPermissionsAsync()
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => {
+        console.log(location)
+      })
     } catch (e) {
       setErr(e);
     }
