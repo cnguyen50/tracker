a8ef1c1a419f3a65d5d565d3ecacaf644acd13fe
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-17 22:29:29 -0800

    removed points array and loop and polyline

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -4,20 +4,6 @@ import MapView, { Polyline } from 'react-native-maps'
 
 
 const Map = () => {
-  let points = []
-  for (let i = 0; i < 20; i++){
-    if (i % 2 === 0) {
-      points.push({
-        latitude: 37.33233 + i * 0.001,
-        longitude: -122.03121 + i * 0.001
-      })
-    } else {
-      points.push({
-        latitude: 37.33233 - i * 0.002,
-        longitude: -122.03121 + i * 0.001
-      })
-    }
-  }
 
   return (
     <MapView 
@@ -29,7 +15,6 @@ const Map = () => {
         longitudeDelta: 0.01
       }}
     >
-      <Polyline coordinates={points} />
     </MapView>
   )
 }
