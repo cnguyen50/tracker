032af8341ccfcb64ee584fe2b652d1d8c3b75ee0
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-21 18:17:17 -0800

    fixed on render issue within useEffect

--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -21,7 +21,7 @@ const changeName = dispatch => (name) => {
   dispatch({ type: 'change_name', payload: name })
 }
 
-const startRecording = dispatch => () => { dispatch({ type: 'stop_recording'})}
+const startRecording = dispatch => () => { dispatch({ type: 'start_recording'})}
 const stopRecording = dispatch => () => { dispatch({ type: 'stop_recording'})}
 
 const addLocation = dispatch => (location, recording) => {
