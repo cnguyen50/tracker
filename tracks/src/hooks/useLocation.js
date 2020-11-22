3c9d37fecd92005911b5e43e963033026bc6f6c0
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-11-21 17:04:34 -0800

    automatic disable

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -1,32 +1,39 @@
 import { useState, useEffect } from 'react'
 import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-location'
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null)
+  const [subscriber, setSubscriber] = useState(null)
   
   const startWatching = async () => {
     try {
       const { granted } = await requestPermissionsAsync();
-    
       if (!granted) {
         throw new Error('Location permission not granted');
       }
+
       await requestPermissionsAsync()
 
-      const subscriber = await watchPositionAsync({
+      const sub = await watchPositionAsync({
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
         }, callback
       )
-    } catch (e) {
-      setErr(e);
+      setSubscriber(sub)
+      } catch (e) {
+        setErr(e);
     }
   };
   
   useEffect(() => {
+    if (shouldTrack) {
     startWatching()
-  }, [])
+    } else {
+      subscriber.remove()
+      setSubscriber(null)
+    }
+  }, [shouldTrack])
 
   return [err]
 
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -9,13 +9,12 @@ import useLocation from '../hooks/useLocation'
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext)
-  const [err] = useLocation(addLocation)
+  const [err] = useLocation(isFocused, addLocation)
 
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2> Create a track</Text>
       <Map />
-
       {err ? <Text>Please enable location services</Text>: null}
     </SafeAreaView>
   )
